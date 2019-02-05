946c411ce759ff689d70be715621425ac14fd2b6
Ryan <ryan.bg.watson@gmail.com>
2019-02-05 10:48:44 -0500

    added routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+
   public ngOnInit() {
 
   }
