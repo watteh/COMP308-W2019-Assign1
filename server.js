d7febf37d4556c198f3bfde355d242af23675d2c
Ryan <ryan.bg.watson@gmail.com>
2019-02-06 12:34:43 -0500

    fix links

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static('./dist/WatsonWebsite'));
 
 app.get('/*', function(req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/<name-of-app>/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/WatsonWebsite/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
